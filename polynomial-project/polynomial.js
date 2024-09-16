63ea026064c7c4d1b5f8fb143edd95f5dbd05526
shanubindal <131860070+shanubindal@users.noreply.github.com>
2024-09-16 12:06:08 +0530

    test case2

--- polynomial-project/polynomial.js
+++ polynomial-project/polynomial.js
@@ -65,26 +65,47 @@ function main(jsonInput) {
 
 const jsonInput = `{
     "keys": {
-        "n": 4,
-        "k": 3
+        "n": 9,
+        "k": 6
     },
     "1": {
         "base": "10",
-        "value": "4"
+        "value": "28735619723837"
     },
     "2": {
-        "base": "2",
-        "value": "111"
+        "base": "16",
+        "value": "1A228867F0CA"
     },
     "3": {
-        "base": "10",
-        "value": "12"
+        "base": "12",
+        "value": "32811A4AA0B7B"
+    },
+    "4": {
+        "base": "11",
+        "value": "917978721331A"
+    },
+    "5": {
+        "base": "16",
+        "value": "1A22886782E1"
     },
     "6": {
-        "base": "4",
-        "value": "213"
+        "base": "10",
+        "value": "28735619654702"
+    },
+    "7": {
+        "base": "14",
+        "value": "71AB5070CC4B"
+    },
+    "8": {
+        "base": "9",
+        "value": "122662581541670"
+    },
+    "9": {
+        "base": "8",
+        "value": "642121030037605"
     }
-}`;
+}
+`;
 
 try {
     const constantTerm = main(jsonInput);
